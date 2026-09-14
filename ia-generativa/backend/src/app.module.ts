203544bc8973fde5f3965004ac9b402903530961
Roberio <roberiojr110@gmail.com>
2026-09-14 20:07:39 -0300

    fix: registra módulo de conversas no AppModule

--- ia-generativa/backend/src/app.module.ts
+++ ia-generativa/backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { ChamadosModule } from './ia/chamados/chamados.module';
 import { IaModule } from './ia/ia.module';
+import { ConversasModule } from './conversas/conversas.module';
 
 @Module({
   imports: [
@@ -10,6 +12,7 @@ import { IaModule } from './ia/ia.module';
     }),
     IaModule,
     ChamadosModule,
+    ConversasModule,
   ],
 })
 export class AppModule {}
